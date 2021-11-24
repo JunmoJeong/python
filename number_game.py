0c4615c61a512ad5fef4900f4c95146994dba151
JunmoJeong <jjmstars7@gmail.com>
2021-11-24 09:59:25 +0900

    python practice

---
+++ number_game.py
@@ -0,0 +1,15 @@
+import random
+
+number = random.randint(1, 99)
+
+
+chance = 10
+count = 0
+
+while count < chance:
+    count += 1
+    user_input = input("몇 일까요?")
+    if number == user_input:
+        print("정답")
+    else:
+        print("아닙니다.")
